3973c6bedfd64843ba9309d6c43ebc1403d6585c
Jaewon <woin2ee@gmail.com>
2022-06-09 14:30:37 +0900

    기본 View 세팅완료

--- AlamofireExample/ViewController.swift
+++ AlamofireExample/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblJoke: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func updateJoke(_ sender: UIButton) {
+        
+    }
 }
-
