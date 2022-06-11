7c5db7bc83718746c4f436ec0964c9a2201fd5f1
Jaewon <woin2ee@gmail.com>
2022-06-11 19:53:53 +0900

    불필요한 코드 제거

--- AlamofireExample/Presentation/HomeScene/HomeViewController.swift
+++ AlamofireExample/Presentation/HomeScene/HomeViewController.swift
@@ -19,19 +19,7 @@ class HomeViewController: UIViewController {
         AF.request("http://api.icndb.com/jokes/random")
             .validate()
             .responseDecodable(of: JokeResponseDTO.self) { response in
-//                self.printToConsole(response)
-                
                 self.lblJoke.text = response.value?.joke.content
             }
-    }
-    
-    func printToConsole<T>(_ response: DataResponse<T, AFError>) {
-        print("response : \(response)")
-        print("response.data : \(String(describing: response.data))")
-        print("response.result : \(response.result)")
-        print("response.request : \(String(describing: response.request))")
-        print("response.response : \(String(describing: response.response))")
-        print("response.description : \(response.description)")
-        print("response.value : \(String(describing: response.value))")
     }
 }
